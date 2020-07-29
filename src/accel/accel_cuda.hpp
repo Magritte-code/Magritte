d3f9f86e7438960b03719dcb58e370a41a2ba2af
FrederikDeCeuster <frederik.deceuster@gmail.com>
2020-07-29 17:06:48 +0200

    added .gitignore

---
+++ src/accel/accel_cuda.hpp
@@ -0,0 +1,8 @@
+//
+// Created by frederik on 29/07/2020.
+//
+
+#ifndef ABSPARAC_ACCEL_CUDA_HPP
+#define ABSPARAC_ACCEL_CUDA_HPP
+
+#endif //ABSPARAC_ACCEL_CUDA_HPP
---
+++ src/types/types.hpp
@@ -0,0 +1,8 @@
+//
+// Created by frederik on 29/07/2020.
+//
+
+#ifndef ABSPARAC_TYPES_HPP
+#define ABSPARAC_TYPES_HPP
+
+#endif //ABSPARAC_TYPES_HPP
