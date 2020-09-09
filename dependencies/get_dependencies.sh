8c1183917057c2769f0dd967443bcf3b879d51b6
FrederikDeCeuster <frederik.deceuster@gmail.com>
2020-09-09 09:23:41 +0200

    Made get_dependencies directory agnostic

--- dependencies/get_dependencies.sh
+++ dependencies/get_dependencies.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Get directory this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Go to this directory
+cd $DIR
+
 # Remove everything
 rm -rf Eigen
 rm -rf pybind11
