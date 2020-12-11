2c099bb72626f2116483949f8c108b2d5c643555
FrederikDeCeuster <frederik.deceuster@gmail.com>
2020-12-11 14:39:26 +0100

    Attempted fix Travis memory issue (21)

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -9,14 +9,20 @@ cd $DIR
 mkdir models
 mkdir results
 
+python3 tests.py
+
+touch models/test.txt
 ls -lh        models
 chmod ugo+rwx models
+
+touch models/tests2.txt
 ls -lh        models
+
+
 
 # Unit tests
 echo "Running unit tests..."
 
-python3 tests.py
 
 # Integration tests
 echo "Running integration tests..."
@@ -24,6 +30,8 @@ echo "Running integration tests..."
 cd $DIR/benchmarks/analytic
 python3 all_constant_single_ray.py         nosave
 # python3 density_distribution_single_ray.py nosave
+
+ls -lh        models
 
 cd $DIR/benchmarks/numeric
 # python3 vanZadelhoff_1_1D.py               nosave
