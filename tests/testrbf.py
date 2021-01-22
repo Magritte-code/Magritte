584c4d576b067b714141d448c994b41b1b893930
Thomas Ceulemans <thomas.ceulemans1@student.kuleuven.be>
2021-01-22 13:48:31 +0100

    Undo previous decision (too small values lead to interpolation result that might not lie between the values of the neighbors...)

--- tests/testrbf.py
+++ tests/testrbf.py
@@ -8,13 +8,13 @@ import healpy as hp
 import scipy.interpolate as spint
 # import statsmodels.api as sm
 
-x=[10,0,0,1]
+x=[1000,0,0,1]
 y=[0,1,0,0]
 z=[0,0,1,0]
 
 vals=[1,4.5,5,5]
 
-rbfi=spint.Rbf(x,y,z,vals,function='gaussian',epsilon=10);
+rbfi=spint.Rbf(x,y,z,vals,function='gaussian',epsilon=5);
 print(rbfi(0,0,0))
 print(rbfi(0,0.5,0))
 print(rbfi.function)
