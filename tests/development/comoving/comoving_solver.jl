a276f7f999a91321a34cc7417695d7341abcfe8c
Thomas Ceulemans <thomas.ceulemans@kuleuven.be>
2022-03-17 11:00:51 +0100

    Improved the boundary conditions; implemented a fully stably fully second order scheme

--- tests/development/comoving/comoving_solver.jl
+++ tests/development/comoving/comoving_solver.jl
@@ -8,7 +8,7 @@ struct Data
 
     currintensity::Vector{Float64}
     allintensities::Array{Float64}
-    bdyintensity::Vector{Float64}
+    backgroundintensity::Array{Float64}
     χ::Array{Float64}
     η::Array{Float64}
     npoints::Int
@@ -20,14 +20,14 @@ struct Data
 
 end
 
-function data(bdyintensity, x, v, χ, η, ν, lineν)
-    currintensity=copy(bdyintensity)
-    nfreqs=length(bdyintensity)
+function data(backgroundintensity, x, v, χ, η, ν, lineν)
+    @views currintensity=copy(backgroundintensity[:,1])
+    nfreqs,_=size(backgroundintensity)
     npoints=length(x)
     allintensities=zeros(Float64, nfreqs, npoints)
-    allintensities[:,1].=bdyintensity
+    @views allintensities[:,1].=currintensity#backgroundintensity[:,1]
 
-    return Data(currintensity, allintensities, bdyintensity, χ, η, npoints, nfreqs, v, x, ν, lineν)
+    return Data(currintensity, allintensities, backgroundintensity, χ, η, npoints, nfreqs, v, x, ν, lineν)
 end
 
 #computes intensity using first order fully explicit discretization
@@ -37,6 +37,7 @@ function firstorderexplicit(previndex, data, forwardfreqdisc::Bool)
     #renaming stuff
     nfreqs=data.nfreqs
     currintensity=data.currintensity
+    bdyintensity=data.backgroundintensity
     η=data.η
     χ=data.χ
     ν=data.ν
@@ -46,9 +47,11 @@ function firstorderexplicit(previndex, data, forwardfreqdisc::Bool)
     if forwardfreqdisc
         @views currintensity[1:(nfreqs-1)].+=(Δx.*(η[1:(nfreqs-1), previndex]-currintensity[1:(nfreqs-1)].*χ[1:(nfreqs-1), previndex])
             +Δv.*Δνterm)#times ν/Δν needs to be added
+        currintensity[nfreqs]=bdyintensity[nfreqs, previndex+1]
     else
         @views currintensity[2:nfreqs].+=(Δx.*(η[2:nfreqs, previndex]-currintensity[2:nfreqs].*χ[2:nfreqs, previndex])
             +Δv.*Δνterm)#times ν/Δν needs to be added
+        currintensity[1]=bdyintensity[1, previndex+1]
     end
     data.allintensities[:,previndex+1]=currintensity;
 
@@ -129,6 +132,7 @@ function firstorderexplicitsecondorderfrequency(previndex, data, forwardfreqdisc
     #renaming stuff
     nfreqs=data.nfreqs
     currintensity=data.currintensity
+    bdyintensity=data.backgroundintensity
     η=data.η
     χ=data.χ
     ν=data.ν
@@ -155,9 +159,11 @@ function firstorderexplicitsecondorderfrequency(previndex, data, forwardfreqdisc
     if forwardfreqdisc
         @views currintensity[1:(nfreqs-2)].+=(Δx.*(η[1:(nfreqs-2), previndex]-currintensity[1:(nfreqs-2)].*χ[1:(nfreqs-2), previndex])
             +Δv.*Δνterm)#times ν/Δν needs to be added
+        @views currintensity[nfreqs-1:nfreqs]=bdyintensity[nfreqs-1:nfreqs, previndex+1]
     else
         @views currintensity[3:nfreqs].+=(Δx.*(η[3:nfreqs, previndex]-currintensity[3:nfreqs].*χ[3:nfreqs, previndex])
             +Δv.*Δνterm)#times ν/Δν needs to be added
+        @views currintensity[1:2]=bdyintensity[1:2, previndex+1]
     end
     data.allintensities[:,previndex+1]=currintensity;
 
@@ -193,6 +199,7 @@ function secondorder(previndex, data, forwardfreqdisc::Bool)
     #renaming stuff
     nfreqs=data.nfreqs
     currintensity=data.currintensity
+    bdyintensity=data.backgroundintensity
     η=data.η
     χ=data.χ
     ν=data.ν
@@ -214,12 +221,15 @@ function secondorder(previndex, data, forwardfreqdisc::Bool)
     #Setup the matrix
     Δνterm2=ones(nfreqs-1).*lineν./(view(ν, 2:nfreqs)-view(ν, 1:(nfreqs-1)))
     diagonal=ones(nfreqs)
+    #correctly applying the boundary conditions also at this point
     if forwardfreqdisc
         @views diagonal[1:(nfreqs-1)].+=Δxdiv2.*χ[1:(nfreqs-1), previndex+1]+Δvdiv2.*Δνterm2
         offdiagonal=-Δvdiv2*Δνterm2
+        @views currintensity[nfreqs]=bdyintensity[nfreqs, previndex+1]
     else
         @views diagonal[2:nfreqs].+=Δxdiv2.*χ[2:nfreqs, previndex+1]-Δvdiv2.*Δνterm2
         offdiagonal=Δvdiv2.*Δνterm2
+        @views currintensity[1]=bdyintensity[1, previndex+1]
     end
 
 
@@ -228,6 +238,7 @@ function secondorder(previndex, data, forwardfreqdisc::Bool)
     matrix=LA.Bidiagonal(diagonal, offdiagonal, upperorlowersymbol)
 
     currintensity.=matrix \ currintensity
+
 
     data.allintensities[:,previndex+1]=currintensity;
     return
@@ -251,6 +262,127 @@ end
 
 
 
+#computes intensity using second order semi implicit discretization (also second order for the frequency derivative)
+function secondorderfull(previndex, data, forwardfreqdisc::Bool)
+    Δxdiv2=(data.x[previndex+1]-data.x[previndex])/2.0#assumes x strictly increasing
+    Δvdiv2=(data.v[previndex+1]-data.v[previndex])/2.0
+    #renaming stuff
+    nfreqs=data.nfreqs
+    currintensity=data.currintensity
+    bdyintensity=data.backgroundintensity
+    η=data.η
+    χ=data.χ
+    ν=data.ν
+    lineν=data.lineν
+
+    if forwardfreqdisc#TODO check
+        Δνsmall=(view(ν, 3:nfreqs)-view(ν, 2:(nfreqs-1)))
+    else
+        Δνsmall=(view(ν, 2:nfreqs-1)-view(ν, 1:(nfreqs-2)))
+    end
+    Δνlarge=(view(ν, 3:nfreqs)-view(ν, 1:(nfreqs-2)))
+    a=-Δνsmall./(Δνlarge.^2-Δνsmall.*Δνlarge)
+    b=Δνlarge./(Δνsmall.*Δνlarge-Δνsmall.^2)
+    c=.-a.-b;
+    # println(stdout, "a,b,c: ", max(a...), ", ", max(b...), ", ", max(c...))
+    # println(1.0 ./max(Δνsmall...))
+
+    if forwardfreqdisc
+        Δνterm=(a.*view(currintensity, 3:nfreqs).+b.*view(currintensity, 2:nfreqs-1).+c.*view(currintensity, 1:nfreqs-2)).*lineν
+    else
+        Δνterm=(-a.*view(currintensity, 1:nfreqs-2).+-b.*view(currintensity, 2:nfreqs-1).+-c.*view(currintensity, 3:nfreqs)).*lineν
+    end
+
+    if forwardfreqdisc
+        @views currintensity[1:(nfreqs-2)].+=(Δxdiv2.*(η[1:(nfreqs-2), previndex]+η[1:(nfreqs-2), previndex+1]-currintensity[1:(nfreqs-2)].*χ[1:(nfreqs-2), previndex])
+            +Δvdiv2.*Δνterm)#times ν/Δν needs to be added
+    else
+        @views currintensity[3:nfreqs].+=(Δxdiv2.*(η[3:nfreqs, previndex]+η[3:nfreqs, previndex+1]-currintensity[3:nfreqs].*χ[3:nfreqs, previndex])
+            +Δvdiv2.*Δνterm)#times ν/Δν needs to be added
+    end
+
+    #setting up the matrix
+    diagonal=ones(nfreqs)
+    offdiagonal=zeros(nfreqs-1)
+    secondoffdiagonal=zeros(nfreqs-2)
+    if forwardfreqdisc
+        @views diagonal[1:(nfreqs-2)].+=Δxdiv2.*χ[1:(nfreqs-2), previndex+1]-Δvdiv2.*c.*lineν
+        @views offdiagonal[1:(nfreqs-2)]=-Δvdiv2.*b.*lineν
+        secondoffdiagonal=-Δvdiv2.*a.*lineν
+
+        #inefficient, as julia stores the entire matrix, but this should work
+        matrix=LA.diagm(0 => diagonal,1=>offdiagonal, 2=>secondoffdiagonal)
+        @views currintensity[nfreqs-1:nfreqs]=bdyintensity[nfreqs-1:nfreqs, previndex+1]
+    else
+        @views diagonal[3:nfreqs].+=Δxdiv2.*χ[3:nfreqs, previndex+1]+Δvdiv2.*c.*lineν
+        @views offdiagonal[3:nfreqs-1]=Δvdiv2.*b.*lineν
+        secondoffdiagonal=Δvdiv2.*a.*lineν
+
+        #inefficient, as julia stores the entire matrix, but this should work
+        matrix=LA.diagm(0 => diagonal,-1=>offdiagonal, -2=>secondoffdiagonal)
+        @views currintensity[1:2]=bdyintensity[1:2, previndex+1]
+    end
+
+
+
+
+    #
+    # Δνterm=(view(currintensity, 2:nfreqs)-view(currintensity, 1:(nfreqs-1))).*lineν./(view(ν, 2:nfreqs)-view(ν, 1:(nfreqs-1)))
+    # #Applying rhs matrix
+    # if forwardfreqdisc
+    #     @views currintensity[1:(nfreqs-1)].+=(Δxdiv2.*(η[1:(nfreqs-1), previndex].+η[1:(nfreqs-1), previndex+1]
+    #                                                   -currintensity[1:(nfreqs-1)].*χ[1:(nfreqs-1), previndex])
+    #                                          +Δvdiv2.*Δνterm)#times ν/Δν needs to be added
+    # else
+    #     @views currintensity[2:nfreqs].+=(Δxdiv2.*(η[2:nfreqs, previndex]+η[2:nfreqs, previndex+1]
+    #                                               -currintensity[2:nfreqs].*χ[2:nfreqs, previndex])
+    #                                      +Δvdiv2.*Δνterm)#times ν/Δν needs to be added
+    # end
+
+    #Solving for lhs matrix
+    #Setup the matrix
+    # Δνterm2=ones(nfreqs-1).*lineν./(view(ν, 2:nfreqs)-view(ν, 1:(nfreqs-1)))
+    # diagonal=ones(nfreqs)
+    # if forwardfreqdisc
+    #     @views diagonal[1:(nfreqs-1)].+=Δxdiv2.*χ[1:(nfreqs-1), previndex+1]+Δvdiv2.*Δνterm2
+    #     offdiagonal=-Δvdiv2*Δνterm2
+    # else
+    #     @views diagonal[2:nfreqs].+=Δxdiv2.*χ[2:nfreqs, previndex+1]-Δvdiv2.*Δνterm2
+    #     offdiagonal=Δvdiv2.*Δνterm2
+    # end
+    #
+    #
+    # upperorlowersymbol=(forwardfreqdisc ? :U : :L)
+    #
+    # matrix=LA.Bidiagonal(diagonal, offdiagonal, upperorlowersymbol)
+
+    currintensity.=matrix \ currintensity
+
+    data.allintensities[:,previndex+1]=currintensity;
+    return
+end
+
+
+function computesingleraysecondorderfull(data::Data)
+    #TODO analyse whether we need some extra points inbetween
+    #use data struct defined here
+    for i ∈ 1:data.npoints-1
+        #compute dv
+
+        dv=data.v[i+1]-data.v[i]
+        forwardfreqdisc = (dv>=0 ?  true : false)
+        #compute the next one
+        secondorderfull(i, data, forwardfreqdisc)
+    end
+    # display(Plots.plot(data.currintensity)
+    return
+end
+
+
+
+
+
+
 #Static frame methods implemented below
 
 #computes intensity using first order fully explicit discretization but with static frequency quadrature
@@ -260,6 +392,7 @@ function firstorderexplicitstatic(previndex, data)
     #renaming stuff
     nfreqs=data.nfreqs
     currintensity=data.currintensity
+    bdyintensity=data.backgroundintensity
     η=data.η
     χ=data.χ
     ν=data.ν
@@ -302,6 +435,7 @@ function secondorderstatic(previndex, data)
     #renaming stuff
     nfreqs=data.nfreqs
     currintensity=data.currintensity
+    bdyintensity=data.backgroundintensity
     η=data.η
     χ=data.χ
     ν=data.ν
--- tests/development/comoving/test.jl
+++ tests/development/comoving/test.jl
@@ -11,24 +11,25 @@ import SpecialFunctions as sf
 ld=TestMolecule.testlinedata()
 println(ld)
 
-factor=1.0;#normally, we should adaptively determine to insert ghost points inbetween, but for simplicity, we just make a more dense discretization
+quadfactor=1
+factor=5.0;#normally, we should adaptively determine to insert ghost points inbetween, but for simplicity, we just make a more dense discretization
 #not the exact settings, but just to test
-npoints   = convert(Int, 301*factor)
+npoints   = convert(Int, 100*factor)
 nrays     = 1
-nquads    = 17
+nquads    = 121*quadfactor
 
 nH2  = 1.0E+12                 # [m^-3]
 nTT  = 1.0E+08                 # [m^-3]
 temp = 4.5E+01                 # [K]
 turb = 0.0E+00                 # [m/s]
-dx   = 1.0E+04/factor          # [m]
-dx   = 3.0e11
+dx   = 1.0E+04/factor        # [m]
+# dx   = 3.0e11
 r_in=10.0
 
 dv   = 2.5E+02 / 300_000_000   # [fraction of speed of light]
-dv   = 2.5E+02 / 300_000_000/ factor   # [fraction of speed of light]
+dv   = 2.5E+03 / 300_000_000/ factor   # [fraction of speed of light]
 # dv   = 0.015
-dv   = 1e-19
+# dv   = 1e-19
 
 L    = dx*(npoints-1)
 vmax = dv*npoints
@@ -49,7 +50,7 @@ println(ld.frequency[1])
 println(dnu)
 
 
-quad_rel_diff=0.25
+quad_rel_diff=0.25/quadfactor
 ν=ld.frequency[1].+(-(nquads-1)/2:(nquads-1)/2).*quad_rel_diff.*dnu
 println(length(ν))
 println(ν)
@@ -60,6 +61,11 @@ middleν=ld.frequency[1]
 νdoppl=reshape(collect([ν[freq]*(1.0+v[point]) for point ∈ 1:npoints for freq in 1:nquads]), nquads, npoints)#ν.*ones(Float64, nquads, npoints).*(1.0 .+v)
 middleνdoppl=reshape(middleν.*(1.0 .+v), 1, npoints)
 doppl_δν=reshape([ν[freq]-middleνdoppl[point] for point ∈ 1:npoints for freq ∈ 1:nquads], nquads, npoints)
+println("dopple freq: ",size(νdoppl))
+Icmb=bdy.(νdoppl)
+println("Icmb: ", size(Icmb))
+println(min(Icmb...))
+println(max(Icmb...))
 # println(νdoppl)
 # println(size(νdoppl))
 # println(size(middleνdoppl))
@@ -138,8 +144,9 @@ end
 
 # v=(-1.0).^(1:npoints)*dv
 
-doppler_shifts=v.*middleν./(quad_rel_diff.*dnu)
-# println(doppler_shifts)
+# doppler_shifts=v.*middleν./(quad_rel_diff.*dnu)#err was amount of bins, not amount of line widths
+doppler_shifts=v.*middleν./dnu
+println(doppler_shifts)
 
 #TODO set boundary intensity
 #set bdy intensity to 1/5 for now
@@ -147,11 +154,11 @@ bdyintensity=bdy.(ν)
 # println(bdyintensity)
 # bdyintensity=zeros(nquads)
 
-data=ComovingSolvers.data(bdyintensity,(0:npoints-1).*dx, v, χ, η, ν, middleν)
+data=ComovingSolvers.data(Icmb,(0:npoints-1).*dx, v, χ, η, ν, middleν)
 ComovingSolvers.computesinglerayfirstorderexplicit(data)
 firstorderintensities=data.allintensities
 
-data2=ComovingSolvers.data(bdyintensity,(0:npoints-1).*dx, v, χ, η, ν, middleν)
+data2=ComovingSolvers.data(Icmb,(0:npoints-1).*dx, v, χ, η, ν, middleν)
 ComovingSolvers.computesingleraysecondorder(data2)
 secondorderintensities=data2.allintensities
 
@@ -159,19 +166,24 @@ secondorderintensities=data2.allintensities
 # ComovingSolvers.computesinglerayfirstorderexplicitupwind(data3)
 # firstorderintensitiesupwind=data3.allintensities
 
-data4=ComovingSolvers.data(bdyintensity,(0:npoints-1).*dx, v, χ, η, ν, middleν)
+data4=ComovingSolvers.data(Icmb,(0:npoints-1).*dx, v, χ, η, ν, middleν)
 ComovingSolvers.computesinglerayfirstorderexplicitsecondorderfrequency(data4)
 secondorderfreq=data4.allintensities
 
 #static solver
-data5=ComovingSolvers.data(bdyintensity,(0:npoints-1).*dx, v, χstatic, ηstatic, ν, middleν)#actually the two last things are filled in wrongly
+data5=ComovingSolvers.data(Icmb,(0:npoints-1).*dx, v, χstatic, ηstatic, ν, middleν)#actually the two last things are filled in wrongly
 ComovingSolvers.computesinglerayfirstorderexplicitstatic(data5)
 staticfreq=data5.allintensities
 
 #second order static solver
-data6=ComovingSolvers.data(bdyintensity,(0:npoints-1).*dx, v, χstatic, ηstatic, ν, middleν)#actually the two last things are filled in wrongly
+data6=ComovingSolvers.data(Icmb,(0:npoints-1).*dx, v, χstatic, ηstatic, ν, middleν)#actually the two last things are filled in wrongly
 ComovingSolvers.computesingleraysecondorderstatic(data6)
 staticfreqsecond=data6.allintensities
+
+#full second order comoving solver
+data7=ComovingSolvers.data(Icmb,(0:npoints-1).*dx, v, χ, η, ν, middleν)
+ComovingSolvers.computesingleraysecondorderfull(data7)
+secondorderfull=data7.allintensities
 
 
 
@@ -182,12 +194,16 @@ println(v[end])
 Plots.plot()
 #multiplication factor because julia plots do not like very small values...
 # Plots.plot([νdoppl[:,end], νdoppl[:,end],νdoppl[:,end]],1e8.*[firstorderintensities[:, npoints], secondorderfreq[:, npoints], secondorderintensities[:,npoints]], label = ["first order" "second order freq" "second order"])
+
 # Plots.savefig("Example static eval")
 # Plots.plot!([νdoppl[:,end], ν[:], ν[:]],1e8.*[Iray, staticfreq[:, npoints], staticfreqsecond[:, npoints]], label = ["analytic" "static first order" "static second order"])
-Plots.plot!([νdoppl[:,end], ν[:]],1e8.*[Iray, staticfreqsecond[:, npoints]], label = ["analytic" "static second order"])
-Plots.vline!([middleνdoppl[end], middleνdoppl[end]-dnu*(1+v[end]), middleνdoppl[end]+dnu*(1+v[end])], label=["shifted line center ±δν" "-δν" "+δν"],legend=:right)
+Plots.plot!([νdoppl[:,end], ν[:], νdoppl[:,end]],1e8.*[Iray, staticfreqsecond[:, npoints], secondorderfull[:, npoints]], label = ["analytic" "static second order" "full second order"])
+
+# Plots.plot([νdoppl[:,end], νdoppl[:,end]],1e8.*[secondorderfreq[:, npoints], Iray], label = ["second order impl" "analytic"])
+Plots.vline!([middleνdoppl[end], middleνdoppl[end]-dnu*(1+v[end]), middleνdoppl[end]+dnu*(1+v[end])], label=["shifted line center ±δν" "-δν" "+δν"],legend=:bottom)
+
 Plots.gui()
-# Plots.savefig("Comparison static analytic")
+# Plots.savefig("Comparison static 2nd vs comoving full 2nd")
 # display(Plots.plot(10e14.*[secondorderintensities[:,npoints]], label = ["second order"]))
 # println(I_.(ν, 11.0e5, 0))
 # Plots.plot(1e5.*[firstorderintensities[:,npoints], secondorderintensities[:,npoints], Iray], label = ["first order" "second order" "analytic"])
